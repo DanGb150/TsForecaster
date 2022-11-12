18dc43f34c6ad31243be111d7cac1b0b37996873
Aurelian Danny <daurelian05@yahoo.com>
2022-11-12 21:29:48 +0200

    transitioned to winter mode

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 const path = require("path");
+const bodyParser = require("body-parser");
 let likelihood = "...";
 const methodOverride = require("method-override");
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //DATABASE COLLECTION IS CHANGEABLE WITH SEASON
 mongoose
-  .connect("mongodb://localhost:27017/NWP", {
+  .connect("mongodb://localhost:27017/NWP-WINTER", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -33,10 +34,13 @@ const nwpSchema = new mongoose.Schema({
     type: [],
   },
 });
-const nwp = mongoose.model("nwp", nwpSchema);
+
+//DATABASE COLLECTION IS CHANGEABLE WITH SEASON
+const nwp = mongoose.model("nwp-winter", nwpSchema);
+//----------------
 
 app.listen(port, () => {
-  console.log(`Listening on port ${3000}`);
+  console.log(`Listening on port ${port}`);
 });
 app.post("/", (req, res) => {
   let likar = req.body;
@@ -53,14 +57,15 @@ app.get("/", (req, res) => {
 app.get("/hail", (req, res) => res.render("hail.ejs"));
 
 app.get("/data", async (req, res) => {
-  const obj = await nwp.find({});
+  const obj = await nwp.findOne();
   res.render("modeldata.ejs", { obj });
 });
+
 app.post("/data", async (req, res) => {
   await nwp.deleteMany({});
-  let nums = Object.keys(req.body);
   let values = Object.values(req.body);
-  const newdata = new nwp({ num: [nums], data: [values] });
+  let nums = Object.keys(req.body);
+  const newdata = new nwp({ num: nums, data: values });
   await newdata.save();
   res.redirect("/data");
 });
